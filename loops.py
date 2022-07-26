0f21c37be78fea513c29e1bfc4b520b99beba135
126-Arvind <109787346+126-Arvind@users.noreply.github.com>
2022-07-26 06:33:56 +0530

    loops concepts

---
+++ loops.py
@@ -0,0 +1,50 @@
+# if 5 == 10 or 4 == 4:
+# #   print("At least one of the statements is true")
+
+# n=6
+# for i in range(1,n+1):
+#     print("*"*i)
+
+ #x = 5
+#   for x in range(1, x + 1):
+#     for y in range(1, x + 1):
+#       print(x, end=' ')
+#     print()
+
+# n=8
+# while(n<=10):
+#     print("hyy")
+#     n+=1
+
+
+# Python 3.x code to demonstrate star pattern
+
+# # Function to demonstrate printing pattern triangle
+# def triangle(n):
+#     # number of spaces
+#     k = n - 1
+#
+#     # outer loop to handle number of rows
+#     for i in range(0, n):
+#
+#         # inner loop to handle number spaces
+#         # values changing acc. to requirement
+#         for j in range(0, k):
+#             print(end=" ")
+#
+#         # decrementing k after each loop
+#         k = k - 1
+#
+#         # inner loop to handle number of columns
+#         # values changing acc. to outer loop
+#         for j in range(0, i + 1):
+#             # printing stars
+#             print("* ", end="")
+#
+#         # ending line after each row
+#         print("\r")
+#
+#
+# # Driver Code
+# n = 5
+# triangle👎
